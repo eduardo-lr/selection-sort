364f569650dec2a32d8c4e8c6492b1b9e4c32ea5
Eduardo Lara Ramirez <eduardo_lr@ciencias.unam.mx>
2021-10-15 19:53:38 -0500

    Documentacion añadida al archivo principal

--- sort.kt
+++ sort.kt
@@ -1,16 +1,11 @@
 import java.util.Arrays
 
-fun main() {	
-	val arreglos = listOf<Array<Int>>(
-		arrayOf(1,2,3,4,5),
-		arrayOf(8,8,8,8,8),
-		arrayOf(5,4,3,2,1),
-		arrayOf(8,1,3,6,9)
-	)
-	for (arreglo in arreglos)
-		println("ordena(${Arrays.toString(arreglo)}) = ${Arrays.toString(ordena(arreglo))}")
-}
-
+/**
+* Esta funcion ordena un arreglo de enteros usando selection sort.
+* El metodo es no destructivo.
+* @param arreglo el arreglo de enteros a ordenar.
+* @return el arreglo ordenado.
+*/
 fun ordena(arreglo: Array<Int>) : Array<Int> {
 	val ordenado = arreglo.copyOf()
 	val n = ordenado.size - 1
@@ -23,8 +18,28 @@ fun ordena(arreglo: Array<Int>) : Array<Int> {
 	return ordenado
 }
 
+/**
+* Funcion auxiliar para intercambiar elementos en un arreglo.
+* @param arreglo el arreglo al cual se le van a intercambiar dos elementos.
+* @param i el indice correspondiente a uno de los elementos a intercambiar.
+* @param j el indice del otro elemento a intercambiar.
+*/
 fun intercambia(arreglo: Array<Int>, i: Int, j: Int) {
 	val temporal = arreglo[i]
 	arreglo[i] = arreglo[j]
 	arreglo[j] = temporal
+}
+
+/**
+* Algunos ejemplos de funcionamiento del programa.
+*/
+fun main() {	
+	val arreglos = listOf<Array<Int>>(
+		arrayOf(1,2,3,4,5),
+		arrayOf(8,8,8,8,8),
+		arrayOf(5,4,3,2,1),
+		arrayOf(8,1,3,6,9)
+	)
+	for (arreglo in arreglos)
+		println("ordena(${Arrays.toString(arreglo)}) = ${Arrays.toString(ordena(arreglo))}")
 }
